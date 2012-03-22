63bf44e5c02837cecb3ab88ceb84b53e8639443a
Luis Gabriel Lima <lampih@gmail.com>
2012-03-21 23:13:32 -0300

    [k-NN] Normalizing data
    Now the data is being normalized in the begining of the process.
    
    Other changes:
        - Moving the data set splitting to a separate function
        - Renaming 'n' parameter to 'k'
        - Renaming 'set' variable to 'data_set'
    
    Signed-off-by: Luis Gabriel Lima <lampih@gmail.com>

--- knn/knn.m
+++ knn/knn.m
@@ -1,10 +1,8 @@
-function [correct, wrong] = knn(n, data, training_rate)
-    set = read_file(data);
-    total = size(set, 1);
-    pivot = round(total * training_rate);
-    training_set = set(1:pivot, :);
-    test_set = set(pivot+1:total,:);
-    [correct, wrong] = run(n, training_set, test_set);
+function [correct, wrong] = knn(k, data, training_rate)
+    data_set = read_file(data);
+    data_set = normalize_values(data_set);
+    [training_set, test_set] = split_set(data_set, training_rate);
+    [correct, wrong] = run(k, training_set, test_set);
 end
 
 function output = read_file(file_name)
@@ -14,15 +12,36 @@ function output = read_file(file_name)
     output = A';
 end
 
-function [correct, wrong] = run(n, training_set, test_set)
+function output = normalize_values(data)
+    output = [];
+
+    % change the class repersentation to a number starting from 1
+    output(:, 1) = data(:, 1) - 64;
+
+    for i = 2 : size(data, 2)
+        column = data(:, i);
+        min_value = min(column);
+        range = max(column) - min_value ;
+        output(:, i) = (column - min_value) / range;
+    end
+end
+
+function [training_set, test_set] = split_set(data_set, training_rate)
+    total = size(data_set, 1);
+    pivot = round(total * training_rate);
+    training_set = data_set(1:pivot, :);
+    test_set = data_set(pivot+1:total,:);
+end
+
+function [correct, wrong] = run(k, training_set, test_set)
     correct = 0;
     wrong = 0;
 
     for i = 1 : size(test_set, 1)
         distances = calc_all_distances(test_set(i, :), training_set);
         sorted_dist = sortrows(distances);
-        nearest_neighbors = sorted_dist(1:n, :);
-        class = classify(n, nearest_neighbors);
+        nearest_neighbors = sorted_dist(1:k, :);
+        class = classify(k, nearest_neighbors);
 
         if class == test_set(i, 1)
             correct += 1;
@@ -51,11 +70,11 @@ function result = euclidian_distance(a, b)
     result = sqrt(acc);
 end
 
-function class = classify(n, nearest_neighbors)
+function class = classify(k, nearest_neighbors)
     sorted = sortrows(nearest_neighbors, 2);
     [values, count] = count_unique(sorted(:,2));
 
-    if length(values) == n
+    if length(values) == k
         class = sorted(1, 2);
     else
         [_, index] = max(count);
