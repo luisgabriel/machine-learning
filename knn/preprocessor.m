af214a2a8ad5105de2d931dc5193ca6f96431001
Luis Gabriel Lima <lampih@gmail.com>
2012-03-24 19:54:54 -0300

    [k-NN] Caching preprocessed data
    Signed-off-by: Luis Gabriel Lima <lampih@gmail.com>

--- knn/preprocessor.m
+++ knn/preprocessor.m
@@ -1,7 +1,42 @@
 function [trainingSet, testSet] = preprocessor(data, trainingRate)
-    dataSet = read_file(data);
-    dataSet = normalize_values(dataSet);
-    [trainingSet, testSet] = split_set(dataSet, trainingRate);
+    k = findstr(data, '.data');
+    prefix = data(1:k-1);
+
+    if is_cached(prefix, trainingRate) != 0
+        [trainingSet, testSet] = load_data(prefix, trainingRate);
+        display('cached');
+    else
+        dataSet = read_file(data);
+        dataSet = normalize_values(dataSet);
+        [trainingSet, testSet] = split_set(dataSet, trainingRate);
+
+        % Caching
+        save_data(prefix, 'all', trainingRate, dataSet);
+        save_data(prefix, 'training', trainingRate, trainingSet);
+        save_data(prefix, 'test', trainingRate, testSet);
+    end
+end
+
+function cached = is_cached(prefix, trainingRate)
+    rate = sprintf('%.2f', trainingRate);
+    fileName1 = strcat(prefix, '-all-', rate, '.data');
+    fileName2 = strcat(prefix, '-training-', rate, '.data');
+    fileName3 = strcat(prefix, '-test-', rate, '.data');
+    cached = exist(fileName1) && exist(fileName2) && exist(fileName3);
+end
+
+function save_data(prefix, sufix, trainingRate, dataSet)
+    rate = sprintf('%.2f', trainingRate);
+    fileName = strcat(prefix, '-', sufix, '-', rate, '.data');
+    dlmwrite(fileName, dataSet);
+end
+
+function [trainingSet, testSet] = load_data(prefix, trainingRate)
+    rate = sprintf('%.2f', trainingRate);
+    fileName = strcat(prefix, '-training-', rate, '.data');
+    trainingSet = dlmread(fileName);
+    fileName = strcat(prefix, '-test-', rate, '.data');
+    testSet = dlmread(fileName);
 end
 
 function output = read_file(fileName)
@@ -39,4 +74,3 @@ function [trainingSet, testSet] = split_set(dataSet, trainingRate)
         testSet = [testSet; content(pivot+1:total,:)];
     end
 end
-
